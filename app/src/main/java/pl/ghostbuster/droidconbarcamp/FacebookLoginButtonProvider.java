2b5ddf0a8a8043c22b00b766b5903adb3d7027e0
ghostbuster91 <kghost0@gmail.com>
2015-12-05 13:00:15 +0100

    create facebook login provider

--- app/src/main/java/pl/ghostbuster/droidconbarcamp/FacebookLoginButtonProvider.java
+++ app/src/main/java/pl/ghostbuster/droidconbarcamp/FacebookLoginButtonProvider.java
@@ -1,4 +1,56 @@
 package pl.ghostbuster.droidconbarcamp;
 
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
 public class FacebookLoginButtonProvider {
+
+    interface SuccessCallback {
+        void onSuccess();
+    }
+
+    interface ErrorCallback {
+        void onError();
+    }
+
+    private CallbackManager callbackManager;
+
+    void addToContainer(ViewGroup container, SuccessCallback successCallback, ErrorCallback errorCallback) {
+        callbackManager = CallbackManager.Factory.create();
+        LayoutInflater inflater = LayoutInflater.from(container.getContext());
+        LoginButton button = (LoginButton) inflater.inflate(R.layout.facebook_login_button, container, false);
+        container.addView(button);
+        button.registerCallback(callbackManager, createFacebookCallback(successCallback, errorCallback));
+    }
+
+    private FacebookCallback<LoginResult> createFacebookCallback(final SuccessCallback successCallback, final ErrorCallback errorCallback) {
+        return new FacebookCallback<LoginResult>() {
+
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                successCallback.onSuccess();
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                errorCallback.onError();
+            }
+        };
+    }
+
+    void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 }
--- app/src/main/java/pl/ghostbuster/droidconbarcamp/MainActivity.java
+++ app/src/main/java/pl/ghostbuster/droidconbarcamp/MainActivity.java
@@ -3,18 +3,14 @@ package pl.ghostbuster.droidconbarcamp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
-import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
-import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    private CallbackManager callbackManager;
+    private FacebookLoginButtonProvider provider = new FacebookLoginButtonProvider();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,22 +18,16 @@ public class MainActivity extends AppCompatActivity {
         FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
 
-        callbackManager = CallbackManager.Factory.create();
         final TextView loginResultsView = (TextView) findViewById(R.id.login_result_descritpion);
-
-        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+        final FrameLayout container = (FrameLayout) findViewById(R.id.facebook_button_container);
+        provider.addToContainer(container, new FacebookLoginButtonProvider.SuccessCallback() {
             @Override
-            public void onSuccess(LoginResult loginResult) {
-                loginResultsView.setText("Success :) ");
+            public void onSuccess() {
+                loginResultsView.setText("Success :)");
             }
-
+        }, new FacebookLoginButtonProvider.ErrorCallback() {
             @Override
-            public void onCancel() {
-            }
-
-            @Override
-            public void onError(FacebookException error) {
+            public void onError() {
                 loginResultsView.setText("Error :(");
             }
         });
@@ -46,6 +36,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        callbackManager.onActivityResult(requestCode, resultCode, data);
+        provider.onActivityResult(requestCode, resultCode, data);
     }
 }
