642dcd6f797c7b17af7c4b2da72351c8692a4483
ghostbuster91 <kghost0@gmail.com>
2015-12-05 11:54:11 +0100

    implement login button

---
+++ app/src/main/java/pl/ghostbuster/droidconbarcamp/FacebookLoginButtonProvider.java
@@ -0,0 +1,4 @@
+package pl.ghostbuster.droidconbarcamp;
+
+public class FacebookLoginButtonProvider {
+}
--- app/src/main/java/pl/ghostbuster/droidconbarcamp/MainActivity.java
+++ app/src/main/java/pl/ghostbuster/droidconbarcamp/MainActivity.java
@@ -1,16 +1,51 @@
 package pl.ghostbuster.droidconbarcamp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+
+        callbackManager = CallbackManager.Factory.create();
+        final TextView loginResultsView = (TextView) findViewById(R.id.login_result_descritpion);
+
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                loginResultsView.setText("Success :) ");
+            }
+
+            @Override
+            public void onCancel() {
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                loginResultsView.setText("Error :(");
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
